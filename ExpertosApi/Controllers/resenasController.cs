d0dffc791b73be5304ad62f37c40b7df9ad34a7c
Emerson Noboa <emerson.noboachi@ug.edu.ec>
2026-02-05 22:08:32 -0500

    controladores

---
+++ ExpertosApi/Controllers/resenasController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackendExpertos.Contexts;
+using BackendExpertos.Models;
+
+namespace ExpertosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class resenasController : ControllerBase
+    {
+        private readonly ExpertoContext _context;
+
+        public resenasController(ExpertoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/resenas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<resena>>> Getresenas()
+        {
+            return await _context.resenas.ToListAsync();
+        }
+
+        // GET: api/resenas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<resena>> Getresena(int id)
+        {
+            var resena = await _context.resenas.FindAsync(id);
+
+            if (resena == null)
+            {
+                return NotFound();
+            }
+
+            return resena;
+        }
+
+        // PUT: api/resenas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putresena(int id, resena resena)
+        {
+            if (id != resena.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(resena).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!resenaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/resenas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<resena>> Postresena(resena resena)
+        {
+            _context.resenas.Add(resena);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Getresena", new { id = resena.id }, resena);
+        }
+
+        // DELETE: api/resenas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deleteresena(int id)
+        {
+            var resena = await _context.resenas.FindAsync(id);
+            if (resena == null)
+            {
+                return NotFound();
+            }
+
+            _context.resenas.Remove(resena);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool resenaExists(int id)
+        {
+            return _context.resenas.Any(e => e.id == id);
+        }
+    }
+}
---
+++ ExpertosApi/Controllers/serviciosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackendExpertos.Contexts;
+using BackendExpertos.Models;
+
+namespace ExpertosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class serviciosController : ControllerBase
+    {
+        private readonly ExpertoContext _context;
+
+        public serviciosController(ExpertoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/servicios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<servicio>>> Getservicios()
+        {
+            return await _context.servicios.ToListAsync();
+        }
+
+        // GET: api/servicios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<servicio>> Getservicio(int id)
+        {
+            var servicio = await _context.servicios.FindAsync(id);
+
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return servicio;
+        }
+
+        // PUT: api/servicios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putservicio(int id, servicio servicio)
+        {
+            if (id != servicio.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(servicio).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!servicioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/servicios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<servicio>> Postservicio(servicio servicio)
+        {
+            _context.servicios.Add(servicio);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Getservicio", new { id = servicio.id }, servicio);
+        }
+
+        // DELETE: api/servicios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deleteservicio(int id)
+        {
+            var servicio = await _context.servicios.FindAsync(id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            _context.servicios.Remove(servicio);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool servicioExists(int id)
+        {
+            return _context.servicios.Any(e => e.id == id);
+        }
+    }
+}
